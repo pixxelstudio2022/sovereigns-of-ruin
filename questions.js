624bad1ed6407a119f808e9a79e9747761709cce
pixxelstudio2022 <132824580+pixxelstudio2022@users.noreply.github.com>
2026-01-03 23:38:36 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,31 @@
+export const saQuizPool = [
+    // GEOGRAPHY & PLACES
+    { q: "Which SA city is known as the 'Mother City'?", a: ["Durban", "Johannesburg", "Cape Town", "Pretoria"], c: 2 },
+    { q: "What is the largest province in South Africa by land area?", a: ["Gauteng", "Northern Cape", "Free State", "Limpopo"], c: 1 },
+    { q: "Which river forms the border between SA and Zimbabwe?", a: ["Vaal", "Orange", "Limpopo", "Zambezi"], c: 2 },
+    { q: "In which province is the Kruger National Park mainly located?", a: ["Mpumalanga", "KZN", "North West", "Western Cape"], c: 0 },
+    { q: "What is the administrative capital of South Africa?", a: ["Cape Town", "Bloemfontein", "Pretoria", "Sandton"], c: 2 },
+    
+    // SPORT (SA PRIDE)
+    { q: "How many Rugby World Cup titles have the Springboks won?", a: ["2", "3", "4", "5"], c: 2 },
+    { q: "Who was the captain of the Springboks in 2019 and 2023?", a: ["Siya Kolisi", "Faf de Klerk", "Eben Etzebeth", "Bryan Habana"], c: 0 },
+    { q: "What is the nickname for the SA national cricket team?", a: ["Bafana Bafana", "Proteas", "Springboks", "Blitzboks"], c: 1 },
+    { q: "In which year did SA host the FIFA World Cup?", a: ["2006", "2008", "2010", "2012"], c: 2 },
+    { q: "Which stadium is shaped like a Calabash?", a: ["Ellis Park", "FNB Stadium", "Moses Mabhida", "Newlands"], c: 1 },
+
+    // FOOD & CULTURE
+    { q: "What is 'Chakalaka' usually served with?", a: ["Cereal", "Pap", "Sushi", "Pasta"], c: 1 },
+    { q: "What do South Africans call a 'Barbecue'?", a: ["Barbie", "Cook-out", "Braai", "Grill-up"], c: 2 },
+    { q: "Which tea is unique to the Cederberg region of SA?", a: ["Earl Grey", "Rooibos", "Green Tea", "Chai"], c: 1 },
+    { q: "What is a 'Bunny Chow'?", a: ["Rabbit stew", "Bread filled with curry", "Pet food", "A type of donut"], c: 1 },
+    { q: "What are 'Amadumbe'?", a: ["Sweet potatoes", "Yams", "Root vegetables", "Corn"], c: 2 },
+
+    // GENERAL KNOWLEDGE & MATH
+    { q: "What is 12 x 8?", a: ["86", "94", "96", "102"], c: 2 },
+    { q: "What is 150 - 75?", a: ["65", "75", "85", "95"], c: 1 },
+    { q: "If a pie costs R15 and you buy 3, how much is it?", a: ["R30", "R45", "R50", "R60"], c: 1 },
+    { q: "What is 1,000 divided by 4?", a: ["200", "250", "300", "500"], c: 1 },
+    { q: "What is 7 squared?", a: ["14", "42", "49", "56"], c: 2 }
+    
+    // ... [I will continue generating the full 200 list for your production file]
+];
